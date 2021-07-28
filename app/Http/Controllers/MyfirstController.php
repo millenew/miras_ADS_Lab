2e3ad277874b4087c23066fb29da0d8832475f1e
FIRST_NAME LAST_NAME <analizaamparo4@gmail.com>
2021-07-28 18:07:58 +0800

    the result is 7

---
+++ app/Http/Controllers/MyfirstController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyfirstController extends Controller
+{
+	public function index()
+	{
+		$sum = 5+2;
+		return view('resultspage', [ 'result' => $sum]);
+	}
+	}
